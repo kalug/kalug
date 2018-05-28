7f4218818f9d40065d8bbd9ecea218eab4752ab1
Shawn Wang <shawn111@gmail.com>
2018-05-28 13:51:15 +0800

    update rss2md.py

--- rss2md.py
+++ rss2md.py
@@ -5,30 +5,39 @@ import os
 import feedparser
 import yaml
 
-from time import strftime
+from time import mktime, strftime
 
 
 def fetch_rss(rss, path):
     feed = feedparser.parse(rss['url'])
     items = feed['items']
-    
+    path = os.path.join(path, rss['id'])
+    os.makedirs(path, exist_ok=True)
+
     for item in items:
         tm = item['published_parsed']
         title = item['title']
         purelink = item['link'].strip('/')
         fileName = "{}_{}.md".format(strftime("%Y%m%d",tm),purelink.split('/')[-1])
-
-        f = open(os.path.join(path, rss['id'], fileName),'w')
+        fn = os.path.join(path, fileName)
+        f = open(fn, 'w')
         value = item['content'][0]['value']
         f.write('''---
 title: "{}"
 date: {}
 type: blog
+author: {}
+link: {}
 layout: post
 comments: true
 ---
 
-'''.format(title, strftime("%Y-%m-%dT%H:%M",tm)))
+'''.format(title, strftime("%Y-%m-%dT%H:%M",tm), rss['author'], item['link']))
         f.write(value)
+        f.close()
+
+        os.utime(fn, (mktime(tm), mktime(tm)))
+
 for rss in yaml.load(open('planet.yml', 'r').read()):
+    #print(rss)
     fetch_rss(rss, "content/planet/")
