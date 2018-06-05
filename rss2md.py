4ad164876aa03465aefd52022944d4da26a26310
Shawn Wang <shawn111@gmail.com>
2018-06-05 09:51:11 +0800

    disable rss2md

--- rss2md.py
+++ rss2md.py
@@ -15,28 +15,29 @@ def fetch_rss(rss, path):
     os.makedirs(path, exist_ok=True)
 
     for item in items:
-        tm = item['published_parsed']
-        title = item['title']
-        purelink = item['link'].strip('/')
-        fileName = "{}_{}.md".format(strftime("%Y%m%d",tm),purelink.split('/')[-1])
-        fn = os.path.join(path, fileName)
-        f = open(fn, 'w')
-        value = item['content'][0]['value']
-        f.write('''---
-title: "{}"
-date: {}
-type: blog
-author: {}
-link: {}
-layout: post
-comments: true
----
+        print(item)
+        #tm = item['published_parsed']
+        #title = item['title']
+        #purelink = item['link'].strip('/')
+        #fileName = "{}_{}.md".format(strftime("%Y%m%d",tm),purelink.split('/')[-1])
+        #fn = os.path.join(path, fileName)
+        #f = open(fn, 'w')
+        #value = item['content'][0]['value']
+        #f.write('''---
+#title: "{}"
+#date: {}
+#type: blog
+#author: {}
+#link: {}
+#layout: post
+#comments: true
+#---
+#
+#'''.format(title, strftime("%Y-%m-%d",tm), rss['author'], item['link']))
+        #f.write(value)
+        #f.close()
 
-'''.format(title, strftime("%Y-%m-%d",tm), rss['author'], item['link']))
-        f.write(value)
-        f.close()
-
-        os.utime(fn, (mktime(tm), mktime(tm)))
+        #os.utime(fn, (mktime(tm), mktime(tm)))
 
 for rss in yaml.load(open('planet.yml', 'r').read()):
     #print(rss)
